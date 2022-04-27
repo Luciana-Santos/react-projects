e0bb603fa51ce2e24cc8ee804402e9f7c5fbcf80
Luciana Santos <luciydss@hotmail.com>
2022-04-27 11:43:57 -0300

    commit inicial

---
+++ tours/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
+
+function App() {
+  return <main>Tours Project Setup</main>;
+}
+
+export default App;
---
+++ tours/src/Tours.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = () => {
+  return <h2>tours component</h2>;
+};
+
+export default Tours;
