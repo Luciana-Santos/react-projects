644e18e6eafd15ece0bd47bd649bd332c8d9b1ad
Luciana Santos <luciydss@hotmail.com>
2022-05-03 23:48:43 -0300

    projeto finalizado

---
+++ menu/README.md
@@ -0,0 +1,5 @@
+# Birthday Reminder
+
+Um simples app que mostra os aniversariantes com um botão para limpar a tela:
+
+![Preview]()
---
+++ menu/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (categorie) => {
+    if (categorie === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === categorie);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
