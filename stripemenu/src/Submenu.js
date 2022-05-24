82afea4e9ddef3af7d64360c2b0f258fb5799df2
Luciana Santos <luciydss@hotmail.com>
2022-05-23 23:44:45 -0300

    iniciando projeto stripe-menu

---
+++ stripemenu/src/Submenu.js
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
+
+const Submenu = () => {
+  const { isSubmenuOpen } = useGlobalContext();
+
+  return (
+    <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+      submenu
+    </aside>
+  );
+};
+
+export default Submenu;
---
+++ stripemenu/src/context.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
